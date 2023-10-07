3364f8111a95568435d9e308759aa263e9864cd2
kevinvillajim <kevinvillajim@hotmail.com>
2023-10-07 01:07:37 -0500

    Configurar URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://kevinvillajim.github.io/random-advices/'
+  base: 'https://kevinvillajim.github.io/random-advices/',
 })
